1a0b3251c3bd9c3e9e6ef619303eeb4a20629baf
Huoxuepeng <99946094+Huoxuepeng@users.noreply.github.com>
2022-07-14 10:43:35 +0300

    Create Task5.java

---
+++ Task5.java
@@ -0,0 +1,14 @@
+package String;
+
+import java.util.Scanner;
+public class ReplaceString {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = new String();
+        str = in.nextLine();
+        String str2 = " ";
+        System.out.println("enter length:" + str.length());
+        for (int i = 0; i < str.length() - 1; i++) {
+            System.out.println((i + 1) + " string is: " + str.charAt(i));
+        }
+        System.out.println(str.replaceAll(" +",""));
